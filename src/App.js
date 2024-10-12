57e7c192e5c312f6354bf364a673098c9202376c
alexandra-adamchyk <aleksandra.adamchik@gmail.com>
2024-10-12 07:08:04 +0200

    App.js, AddFavourites.js, MovieList.js, MovieListHeading.js, RemoveFavourites.js, SearchBox.js were created

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=5e79f1d3b68cbd871e7f3eae35b38742`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
-//function component, returns list of movies,
-//for each of the movies we render a div
-//inside a div is an image tag, which displays the movie poster  
 const MovieList = (props) => {
-    const baseImgUrl500 = 'https://api.themoviedb.org/3/movie/{movie_id}/images';
-    return (
-        <>
-        {props.movies.map((movie, index)=> 
-            <div>
-            {/* <img src={baseImgUrl500 + movie.poster_path} alt="movie"></img>  */
-            <img src={movie.Id} alt='movie'></img>}
-            </div>)}
-        </>
-    )
-}
+	const FavouriteComponent = props.favouriteComponent;
+
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+						<FavouriteComponent />
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
 
 export default MovieList;
